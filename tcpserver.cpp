e9de45fc34a3dc081c2248773bccb1a7aa9e3da5
Kedong Shao <kshao5@illinois.edu>
2022-02-13 15:41:16 -0800

    first commit

---
+++ tcpserver.cpp
@@ -0,0 +1,76 @@
+#include "tcpserver.h"
+
+TcpServer::TcpServer(QObject *parent)
+    : QObject{parent}
+{
+    connect(&_server, &QTcpServer::newConnection, this, &TcpServer::onNewConnection);
+    connect(this, &TcpServer::newMessage, this, &TcpServer::onNewMessage);
+
+    // listen on custom port 45000. if succeed
+    if (_server.listen(QHostAddress::Any, 45000)) {
+        qInfo() <<"Listening...";
+    }
+}
+
+void TcpServer::sendMessage(const QString &message)
+{
+    emit newMessage("Server: " + message.toUtf8());
+}
+
+void TcpServer::onNewConnection()
+{
+    // get the next client that is waiting to connect to our server
+    const auto client = _server.nextPendingConnection();
+    if (client == nullptr) {
+        return;
+    }
+
+    qInfo() << "New client connected.";
+
+    // use this hashmap to access our clients
+    _clients.insert(this ->getClientKey(client), client);
+
+    // readyRead: when clients send message to us and we are ready to read
+    connect(client, &QTcpSocket::readyRead, this, &TcpServer::onReadyRead);
+
+    // when client is disconnect, need to remove that client from hash map
+    connect(client, &QTcpSocket::disconnected, this, &TcpServer::onDisconnected);
+}
+
+void TcpServer::onReadyRead()
+{
+    // we can use sender() to get pointer to client object and read message from client object
+    const auto client = qobject_cast<QTcpSocket*>(sender());
+    if (client == nullptr) {
+        return;
+    }
+
+    const auto message = this -> getClientKey(client).toUtf8() + ": " + client -> readAll();
+
+    // send message to other client and show in our user interface
+    emit newMessage(message);
+}
+
+void TcpServer::onDisconnected()
+{
+    const auto client = qobject_cast<QTcpSocket*>(sender());
+    if (client == nullptr) {
+        return;
+    }
+
+    _clients.remove(this -> getClientKey(client));
+}
+
+void TcpServer::onNewMessage(const QByteArray &ba)
+{
+    for (const auto &client : qAsConst(_clients)) {
+        client -> write(ba);
+        client -> flush();
+    }
+}
+
+QString TcpServer::getClientKey(const QTcpSocket *client) const
+{
+    // identify our client using address and port
+    return client -> peerAddress().toString().append(":").append(QString::number(client -> peerPort()));
+}
---
+++ tcpserver.h
@@ -0,0 +1,37 @@
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <QObject>
+// for server
+#include <QTcpServer>
+// for client
+#include <QTcpSocket>
+#include <QHash>
+
+class TcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TcpServer(QObject *parent = nullptr);
+
+signals:
+    void newMessage(const QByteArray &message);
+
+public slots:
+    void sendMessage(const QString &message);
+
+private slots:
+    void onNewConnection();
+    void onReadyRead();
+    void onNewMessage(const QByteArray &ba);
+    void onDisconnected();
+
+private:
+    QString getClientKey(const QTcpSocket * client) const;
+
+private:
+    QTcpServer _server;
+    QHash<QString, QTcpSocket*> _clients;
+};
+
+#endif // TCPSERVER_H
